a5604e754ccf0cb984021b7b27dd586823203643
kevin fajardo <kevinfajardo1103@gmail.com>
2024-09-06 13:34:09 -0400

    Moving Things With javascript

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.background = '#FF69B4'
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+    // console.log(event);
+
+});
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+
+
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
